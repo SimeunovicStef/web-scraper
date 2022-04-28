094dd1ea3a895e90cd54e5f46a1651b8f80eb8c5
SimeunovicStef <eskimofrogster@gmail.com>
2022-04-28 23:07:25 +0200

    code & functions

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
 
+
+const url = 'https://www.games-workshop.com/en-EU/Home'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+        $('.gw', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+            console.log(articles)
+
+        }).catch(err => console.log(err))
+    })
+
+
 const app = express()
 
-app.listen (PORT, () => console.log(`server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
